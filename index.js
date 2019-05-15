7385bdf5bd3429adb78cee6cf51d563002e5155d
thuanvuong164999 <dungthuan2468@gmail.com>
2019-05-15 21:12:14 +0700

    project

---
+++ index.js
@@ -0,0 +1,27 @@
+let pg = require('pg')
+let client = new pg.Client(
+    {
+        user: 'postgres',
+        password: 'postgres',
+        database: 'csv',
+        host: 'localhost',
+        port: 5432
+    }
+)
+
+client.connect()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    client.query('select * from csv', function(err, result){
+        res.send(JSON.stringify(result.rows))
+        client.end()
+    })
+})
+
+app.listen(port, () => {
+    console.log('asdasda')
+})
